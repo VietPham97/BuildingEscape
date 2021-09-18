6d44741313fd7acb0072258af0f2dce8a96e9633
Viet Pham <vietpham1097@gmail.com>
2021-09-19 00:08:54 +0700

    Create CoreGameMode c++ component and remove previous scripts

---
+++ Source/BuildingEscape/GameMode/CoreGameMode.cpp
@@ -0,0 +1,5 @@
+// Created by Pham Hoang Viet in 2021.
+
+
+#include "GameMode/CoreGameMode.h"
+
---
+++ Source/BuildingEscape/GameMode/CoreGameMode.h
@@ -0,0 +1,17 @@
+// Created by Pham Hoang Viet in 2021.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoreGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_API ACoreGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
